13f6f05a00d4dcc63792eadd530008da12c47371
dklounge <daviddarden11@gmail.com>
2013-10-02 10:25:45 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Day2PrayerWall::Application.config.secret_key_base = 'ee73dc2fd26e1a1de72e5fbcd9020197fc47a9eda0a249cd78b7e438ac968f510120abdd9582dc753d97949b2b4e2781eca9ea78e058be8695fbb02b9bbe3a3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Day2PrayerWall::Application.config.session_store :cookie_store, key: '_day_2_prayer_wall_session'
