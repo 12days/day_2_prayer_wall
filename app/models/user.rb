14b348d16c1bdb3f5274c17d4c979ea7c98d5cee
Joshua Wilborn <Joshua.J.Wilborn@gmail.com>
2013-10-02 17:35:48 -0700

    user change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   validates_confirmation_of :password, message: "should match confirmation", if: :password
+
+  has_many :posts
+  has_many :prayers
 end
